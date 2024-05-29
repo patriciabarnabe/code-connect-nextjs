afb4036568c0ae6dcc81e329d4efb6145facbe18
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-29 00:39:57 -0300

    Creating avatar and cardpost components

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,10 +1,11 @@
 import styles from "./aside.module.css";
 import Image from "next/image";
+import logo from "../../../public/logo.svg";
 
 export const Aside = () => {
   return (
     <aside className={styles.aside}>
-      <img src="/logo.svg" alt="Logo da Code Connect" />
+      <Image src={logo} alt="Logo da Code Connect" width={150} height={50} />
     </aside>
   );
 };
---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Avatar } from "../Avatar";
+
+export const CardPost = ({ post }) => {
+  return (
+    <article>
+      <header>
+        <figure>
+          <Image src={post.cover} alt={post.title} width={438} height={133} />
+        </figure>
+      </header>
+      <section>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+      </section>
+      <footer>
+        <Avatar name={post.author.username} imageSrc={post.author.avatar} />
+      </footer>
+    </article>
+  );
+};
