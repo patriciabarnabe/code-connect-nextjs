b08dee2ddf7ceec7c57d6c73473050643bb14b5f
patriciabarnabe <patbarnabe5@gmail.com>
2024-06-03 16:17:52 -0300

    Paginate and adding more styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css";
+import Link from "next/link";
 
-async function getAllPosts() {
+async function getAllPosts(page) {
   try {
-    const response = await fetch("http://localhost:3042/posts");
+    const response = await fetch(
+      `http://localhost:3042/posts?_page=${page}&_per_page=6`
+    );
     if (!response.ok) throw new Error("Falha na rede");
     logger.info("Posts obtidos com sucesso!");
     return response.json();
@@ -14,14 +17,29 @@ async function getAllPosts() {
   }
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({ searchParams }) {
+  // Paginação baseada nas queries da URL
+  const currentPage = searchParams?.page || 1;
+
+  //Renomeando a propriedade 'data' que vem da API para 'posts'
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
 
   return (
     <main className={styles.grid}>
       {posts.map((post) => (
         <CardPost key={post.id} post={post} />
       ))}
+      {/* Lógica de renderização condicional */}
+      {prev && (
+        <Link href={`/?page=${prev}`} className={styles.links}>
+          Página anterior
+        </Link>
+      )}
+      {next && (
+        <Link href={`/?page=${next}`} className={styles.links}>
+          Próxima página
+        </Link>
+      )}
     </main>
   );
 }
